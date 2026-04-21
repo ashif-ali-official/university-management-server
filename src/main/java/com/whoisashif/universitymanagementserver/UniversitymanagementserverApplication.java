bc4f064bab969967c160806c1581d12cec390f2a
Ashif Ali <ashif.mac@Ashifs-MacBook-Air.local>
2026-04-21 18:00:53 +0530

    Initial Commit of Project

---
+++ src/main/java/com/whoisashif/universitymanagementserver/UniversitymanagementserverApplication.java
@@ -0,0 +1,13 @@
+package com.whoisashif.universitymanagementserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversitymanagementserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversitymanagementserverApplication.class, args);
+	}
+
+}
